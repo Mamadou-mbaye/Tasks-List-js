909792609188246209de38b646568cb1e99f7787
Mamadou-mbaye <153839794+Mamadou-mbaye@users.noreply.github.com>
2024-05-04 20:50:48 +0200

    file css rifatto e js modificato in modo che ogni nuovo task aggiunto se posizione sopra le altre

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
-const container = document.querySelector("#container");
 const input = document.getElementById("input");
 const buttone = document.getElementById("buttone");
 const reset_tasks = document.getElementById("btn-reset");
 const tasks = document.querySelector("#tasks");
-const ul = document.createElement("ul");
 
 buttone.addEventListener("click", addTask);
-input.addEventListener("keypress", (event) => {
-  if (event.key === "Enter") addTask();
-});
+
 reset_tasks.addEventListener("click", () => {
   const confirmation = prompt("Reset Tasks List? (yes/no)");
   if (confirmation.toLowerCase().trim() === "yes") {
-    ul.innerHTML = "";
+    tasks.innerHTML = "";
     localStorage.removeItem("tasks");
   }
 });
@@ -27,23 +23,24 @@ function addTask() {
 }
 
 function createTaskElement(taskText) {
-  const li = document.createElement("li");
-  li.innerHTML =
-    "<span>" +
-    taskText +
-    "</span>" +
-    '<input type="button" value="delete" class="delete-button">';
-  ul.appendChild(li);
-  tasks.appendChild(ul);
+  const p = document.createElement("p");
+  p.className = "list-item";
+  p.innerHTML = `<p class="item">${taskText}</p> <button class="delete-task">delete task</button>`;
+  // Inserisce il nuovo task come primo elemento all'interno del contenitore delle tasks
+  if (tasks.firstChild) {
+    tasks.insertBefore(p, tasks.firstChild);
+  } else {
+    tasks.appendChild(p);
+  }
 
-  const deleteButtons = document.querySelectorAll(".delete-button");
+  const deleteButtons = document.querySelectorAll(".delete-task");
   deleteButtons.forEach((button) => {
     button.addEventListener("click", deleteTask);
   });
 }
 
 function deleteTask() {
-  const taskText = this.parentNode.querySelector("span").innerText;
+  const taskText = this.parentNode.querySelector("p").innerText;
   this.parentNode.remove();
   let tasksArray = JSON.parse(localStorage.getItem("tasks")) || [];
   const newTasksArray = tasksArray.filter((task) => task !== taskText);
